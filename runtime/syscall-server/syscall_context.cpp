47b10f9a6ff6475b2271ce9dae5cb898f20d4f7e
Sy03 <1370724210@qq.com>
2025-11-12 01:20:08 +0800

    fix

--- runtime/syscall-server/syscall_context.cpp
+++ runtime/syscall-server/syscall_context.cpp
@@ -328,7 +328,9 @@ int syscall_context::create_kernel_bpf_prog_in_userspace(int cmd,
 
 long syscall_context::handle_sysbpf(int cmd, union bpf_attr *attr, size_t size)
 {
-	if (!enable_mock || initializing_cuda || !enable_mock_after_initialized)
+	// Always intercept BPF syscalls unless explicitly configured to use
+	// kernel
+	if (!enable_mock || initializing_cuda)
 		return orig_syscall_fn(__NR_bpf, (long)cmd,
 				       (long)(uintptr_t)attr, (long)size);
 	try_startup();
